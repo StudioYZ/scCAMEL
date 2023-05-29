023a4ca3b2a131833b6f7bc80e734e3a5f100432
YZ_studio <100945518+StudioYZ@users.noreply.github.com>
2023-05-29 12:13:31 +0200

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -17,8 +17,8 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'scCAMELi'
-copyright = '2022, Yizhou Hu, YZstudio, Ernfors lab, Karolisnka Inst.'
+project = 'scCAMEL'
+copyright = '2023, Yizhou Hu, YZstudio, Ernfors lab, Karolisnka Inst.'
 author = 'Yizhou Hu'
 
 # The full version, including alpha/beta/rc tags
